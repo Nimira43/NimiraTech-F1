e92bd3f2ee1e0be1893fedbba17c82ce92ba4a2a
Nick Rabone <n.rabone72@aol.com>
2024-06-14 07:56:35 +0100

    Slideshow completed

--- script.js
+++ script.js
@@ -1,12 +1,32 @@
 const slideshowDivs = () => {
     for (let i = 1; i <= 5; i++) {
         const div = document.createElement('div')
-        div.style.backgroundImage = 
-            `
-            url(images/slideshow/bg-${i}.jpg)
-            `
-            document.querySelector('.slideshow').appendChild(div)
+        div.style.backgroundImage = `url(images/slideshow/bg-${i}.jpg)`
+        
+        i === 1 && div.classList.add('change')
+        
+        document.querySelector('.slideshow').appendChild(div)
     }
 }
 
 slideshowDivs()
+
+const divs = document.querySelectorAll('.slideshow div')
+
+let a = 0
+
+const slideshow = () => {
+    setInterval(() => {
+        a++
+        const div = document.querySelector('.slideshow .change')
+        div.classList.remove('change')
+        if (a < divs.length) {
+            div.nextElementSibling.classList.add('change')
+        } else {
+            divs[0].classList.add('change')
+            a = 1
+        }
+    }, 5000)
+}
+
+slideshow()
